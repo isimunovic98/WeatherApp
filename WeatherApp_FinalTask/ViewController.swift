26967b65f2002ebcbfd48f637d57383f69a8e5aa
Ivan Simunovic <ivan.simunovic9898@gmail.com>
2021-01-11 17:32:01 +0100

    Initial Commit

---
+++ WeatherApp_FinalTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp_FinalTask
+//
+//  Created by Ivan Simunovic on 11.01.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
