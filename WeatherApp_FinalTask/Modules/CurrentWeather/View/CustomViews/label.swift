db768933c55d33c3118d6a836fe3c188907c784e
Ivan Simunovic <ivan.simunovic9898@gmail.com>
2021-01-14 20:03:42 +0100

    other changes

---
+++ WeatherApp_FinalTask/Modules/CurrentWeather/View/CustomViews/label.swift
@@ -0,0 +1,20 @@
+//
+//  label.swift
+//  WeatherApp_FinalTask
+//
+//  Created by Ivan Simunovic on 12.01.2021..
+//
+
+import UIKit
+
+class label: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- WeatherApp_FinalTask/SupportFiles/AppDelegate.swift
+++ WeatherApp_FinalTask/SupportFiles/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let navigationController = UINavigationController(rootViewController: ViewController())
+        let navigationController = UINavigationController(rootViewController: CurrentWeatherViewController())
+        navigationController.navigationBar.isHidden = true
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
