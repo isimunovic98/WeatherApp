d2aacd7c2f9d665f2c602ed5844bd844c50e260a
Ivan Simunovic <ivan.simunovic9898@gmail.com>
2021-01-17 13:58:00 +0100

    added viewModel to VC

--- WeatherApp_FinalTask/Modules/CurrentWeather/View/ViewController/CurrentWeatherViewController.swift
+++ WeatherApp_FinalTask/Modules/CurrentWeather/View/ViewController/CurrentWeatherViewController.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 import SnapKit
+import Combine
 
 class CurrentWeatherViewController: UIViewController {
     
     //MARK: Properties
+    var disposeBag = Set<AnyCancellable>()
+    
+    private var viewModel: CurrentWeatherViewModel
+    
     let currentWeatherView: CurrentWeatherView = {
         let view = CurrentWeatherView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -23,13 +28,22 @@ class CurrentWeatherViewController: UIViewController {
         return searchBar
     }()
     
+    init(viewModel: CurrentWeatherViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
         setupView()
+        setupBindings()
     }
 }
-
 //MARK: - UI Setup
 private extension CurrentWeatherViewController {
     func setupView() {
@@ -38,13 +52,14 @@ private extension CurrentWeatherViewController {
         
         setupAppearance()
         setupLayout()
-    }
-    
-    func setupAppearance() {
-        view.backgroundColor = .gray
-    }
-    
-    func setupLayout() {
+     }
+
+     func setupAppearance() {
+         view.backgroundColor = .white
+         view.backgroundColor = .gray
+     }
+
+     func setupLayout() {
         searchBar.snp.makeConstraints { (make) in
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.leading.trailing.equalTo(view)
@@ -55,11 +70,26 @@ private extension CurrentWeatherViewController {
             make.leading.bottom.trailing.equalTo(view)
         }
     }
+    
+    func reloadView() {
+        guard let screenData = viewModel.screenData else { return }
+        currentWeatherView.configure(with: screenData)
+    }
 }
-
 extension CurrentWeatherViewController {
+    func setupBindings() {
+        let dataLoader = viewModel.initializeScreenData(for: viewModel.loadData)
+        dataLoader.store(in: &disposeBag)
+        
+        viewModel.screenDataReady
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: RunLoop.main)
+            .sink(receiveValue: { [weak self] _ in
+                self?.reloadView()
+            })
+            .store(in: &disposeBag)
+    }
 }
-
 extension CurrentWeatherViewController: UISearchBarDelegate {
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         let searchViewController = SearchViewController()
